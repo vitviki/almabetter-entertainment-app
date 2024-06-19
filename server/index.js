32d0183b9492973d6706469d2a3409f77abb41e2
vitviki <vtyagi1991@gmail.com>
2024-06-19 11:10:49 +0530

    added cors header options

--- server/index.js
+++ server/index.js
@@ -35,7 +35,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
-app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // API routes.
