f7a40457d1f047a9784d54f76dded36e3d706b86
vitviki <vtyagi1991@gmail.com>
2024-06-19 11:59:28 +0530

    added cors options to allow all

--- server/index.js
+++ server/index.js
@@ -21,6 +21,10 @@ const storage = multer.diskStorage({
   },
 });
 
+const corsOptions = {
+  credentials: false,
+};
+
 // Middleware for file upload
 const upload = multer({ storage });
 
@@ -34,7 +38,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", "*");
