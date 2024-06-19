92a9e37bfff416d3b35bf315f4f680bc5a4dd335
vitviki <vtyagi1991@gmail.com>
2024-06-19 12:02:24 +0530

    added custom access point

--- server/index.js
+++ server/index.js
@@ -40,8 +40,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", false);
+  res.header("Access-Control-Allow-Origin", "http://http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
   res.header(
     "Access-Control-Allow-Headers",
