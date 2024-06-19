d9df9954d2a0ddc019825a40cc716215ce1a7e8d
vitviki <vtyagi1991@gmail.com>
2024-06-19 11:30:37 +0530

    added cors headers in vercel.json

--- server/index.js
+++ server/index.js
@@ -36,11 +36,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
   next();
 });
