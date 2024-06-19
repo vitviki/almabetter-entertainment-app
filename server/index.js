2640de6bed0165a385f94b14702f6da087e3f064
vitviki <vtyagi1991@gmail.com>
2024-06-19 12:04:49 +0530

    added access point for all

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", false);
-  res.header("Access-Control-Allow-Origin", "http://http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
   res.header(
     "Access-Control-Allow-Headers",
