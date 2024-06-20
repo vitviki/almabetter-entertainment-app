8f0d1f1f26034b4dbf96023ca38682b1fa4efd3d
vitviki <vtyagi1991@gmail.com>
2024-06-20 16:54:28 +0530

    added timestamps in user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,34 +1,39 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicturePath: {
+      type: String,
+      default: "",
+    },
+    favoriteMovies: {
+      type: Array,
+      default: [],
+    },
+    favortiteTVs: {
+      type: Array,
+      default: [],
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePicturePath: {
-    type: String,
-    default: "",
-  },
-  favoriteMovies: {
-    type: Array,
-    default: [],
-  },
-  favortiteTVs: {
-    type: Array,
-    default: [],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const User = mongoose.model("User", userSchema);
